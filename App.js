31ecfe6afe1d7ba35688ec484d2a977f8583236f
GooglyEy3s <91564775+GooglyEy3s@users.noreply.github.com>
2023-11-16 14:27:07 -0300

    Comit

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HelloWorld from './components/01_HelloWorld/01HelloWorld';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <HelloWorld />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
