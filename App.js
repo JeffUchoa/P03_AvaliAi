bdb16622ce7e23782899b4116973424650adeb67
claralves <claralves2002@gmail.com>
2023-11-19 00:04:20 -0300

    Tela de cadastro

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './components/Login/Login';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Login from "./components/Login/Login";
+import CadastrarUsuario from "./components/Login/CadastrarUsuario";
 
 export default function App() {
   return (
-    <Login />
+    // <Login />
+    <CadastrarUsuario />
   );
 }
-
