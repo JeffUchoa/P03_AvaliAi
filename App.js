85912d2fa121a3ffa6c4b9ee340196af987d73fc
claralves <claralves2002@gmail.com>
2023-11-23 15:07:48 -0300

    Elenco

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
     // <Login />
     // <CadastrarUsuario />
     // <Carrosel />
-    // <Pesquisar />
-    <Filme />
+    <Pesquisar />
+    // <Filme />
   );
 }
